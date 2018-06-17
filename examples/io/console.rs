31e1cf71d90d026aae77f6425e9ed162454f79db
yhoazk <yhoazk@gmail.com>
2018-06-17 16:30:10 +0200

    Print formating

---
+++ examples/io/console.rs
@@ -0,0 +1,39 @@
+//use std::fmt;
+
+fn main(){
+   // This is a common way to import only the names we will
+   // use in the program. It's also possible to import all the 
+   // pkg and then use name resolution or simply use a * to
+   // throw all the names in this name scope.
+   // use::io::*;
+    use std::io::{stdin, stdout};
+   // String formatting:
+    print!("{}:{}\n", "Sample", "string");
+    print!("{} - {}, {1}\n", "First", "second");
+    print!("{}:{}::{1}", "Sample", "string");
+    println!("");
+    println!("");
+    // print numbers
+    println!("Int Number: {}", 12);
+    println!("Float number: {}", 3.1425928);
+    println!("Hex 0x{0} to decimal number {1}", format!("{:x}", 0x10), 0x10);
+    println!("Bin 0b{0} to decimal number {1}", format!("{:b}", 0b10), 0b10);
+    // Getting user input:
+        // Create a mutable object, by default the variables are non
+        // mutable
+    let mut s = String::new();
+    print!("Give some input bitch:");
+    // Flushing the stdout, as in C/C++ the stdout is
+    // flished only with new line or with a full buffer.
+    // WTF this does not work??
+    //stdout().flush();
+    stdin().read_line(&mut s).unwrap();
+    // What is the unwrap?
+        /*
+
+        */
+    println!("Input string {}", s);
+   // let mut line = String::new();
+   // println!("{:?}", std::io::stdin().read_line(&mut line));
+   // println!("[{}]", line)
+}
---
+++ keywords/rust_keywords.md
@@ -0,0 +1,37 @@
+# Keyworkds
+
+## fn
+
+This declares a function. Like in C, a program needs an entry 
+point, as is C is a function called `main`. It may be a library.
+
+
+
+
+## IO
+
+### Printing to stdout
+
+```
+print!("String");
+```
+
+In rust there is NO print function, is a macro for this we need
+to add an exclamation mark `print!`. Rust always distinguishes 
+case. Then `Print` is differen from `print`.
+
+For string formating curly braces are used to substitute the
+strings given as parameters for the print macro.
+
+The input arguments are given as a zero based list of strings.
+And the placeholders can be numbered to replace the string with 
+an instance of that list. For example:
+```
+fn main(){
+    print!("{} - {}, {1}\n", "First", "second");
+}
+```
+The result is:
+```
+First - second, second
+```
